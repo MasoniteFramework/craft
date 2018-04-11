6361c297efbd69948284574c2f62edcc4e6131ba
Joseph Mancuso <idmann509@gmail.com>
2018-04-11 12:22:27 -0400

    bumped version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="masonite-cli",
-    version='1.1.2',
+    version='1.1.3',
     packages=[
         'masonite_cli',
         'masonite_cli.commands',
