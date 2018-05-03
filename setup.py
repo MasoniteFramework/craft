346932f1aacdee66c91a89d8e29e537d3f58ef98
Abram C. Isola <abram@isola.mn>
2018-05-03 10:45:44 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="masonite-cli",
-    version='2.0.0',
+    version='2.0.1',
     packages=[
         'masonite_cli',
         'masonite_cli.commands',
