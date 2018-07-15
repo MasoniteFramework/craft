3ab9023c01388141a0daa4578f2f6edf1c079e23
Joseph Mancuso <idmann509@gmail.com>
2018-07-14 21:47:38 -0400

    added better cleo dependency versions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="masonite-cli",
-    version='2.0.9',
+    version='2.0.10',
     packages=[
         'masonite_cli',
         'masonite_cli.commands',
@@ -10,7 +10,7 @@ setup(
     ],
     py_modules=['masonite_cli'],
     install_requires=[
-        'cleo==0.6.5',
+        'cleo>=0.6,<=0.6.99',
         'cryptography',
         'requests==2.19.1',
     ],
