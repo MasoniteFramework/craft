79039af8142ecee03b0bee8f8dd4f96b27c2c624
Joseph Mancuso <idmann509@gmail.com>
2019-11-06 22:12:49 -0500

    updated new command to work for cookie cutter application

--- masonite_cli/commands/NewCommand.py
+++ masonite_cli/commands/NewCommand.py
@@ -32,15 +32,15 @@ class NewCommand(Command):
             try:
                 if branch != 'False':
                     get_branch = requests.get(
-                        'https://api.github.com/repos/MasoniteFramework/masonite/branches/{0}'.format(branch))
+                        'https://api.github.com/repos/MasoniteFramework/cookie-cutter/branches/{0}'.format(branch))
 
                     if 'name' not in get_branch.json():
                         return self.comment('Branch {0} does not exist.'.format(branch))
 
-                    zipball = 'https://github.com/MasoniteFramework/masonite/archive/{0}.zip'.format(branch)
+                    zipball = 'https://github.com/MasoniteFramework/cookie-cutter/archive/{0}.zip'.format(branch)
                 elif version != 'False':
                     get_zip_url = requests.get(
-                        'https://api.github.com/repos/MasoniteFramework/masonite/releases')
+                        'https://api.github.com/repos/MasoniteFramework/cookie-cutter/releases')
                     zipball = False
 
                     for release in get_zip_url.json():
@@ -54,17 +54,17 @@ class NewCommand(Command):
                         return self.info('Version {0} could not be found'.format(version))
                 else:
                     get_zip_url = requests.get(
-                        'https://api.github.com/repos/MasoniteFramework/masonite/releases')
+                        'https://api.github.com/repos/MasoniteFramework/cookie-cutter/releases')
                     tags = []
 
                     for release in get_zip_url.json():
                         if release['prerelease'] is False:
                             tags.append(release['tag_name'].replace('v', ''))
-
+                            
                     tags = sorted(tags, key=lambda v: [int(i) for i in v.split('.')], reverse=True)
 
                     get_zip_url = requests.get(
-                        'https://api.github.com/repos/MasoniteFramework/masonite/releases/tags/v{0}'.format(tags[0]))
+                        'https://api.github.com/repos/MasoniteFramework/cookie-cutter/releases/tags/v{0}'.format(tags[0]))
 
                     zipball = get_zip_url.json()['zipball_url']
             except TypeError:
@@ -86,7 +86,7 @@ class NewCommand(Command):
                     zfile.extractall(os.getcwd())
 
             for directory in os.listdir(os.getcwd()):
-                if directory.startswith('MasoniteFramework-masonite') or directory.startswith('masonite-'):
+                if directory.startswith('MasoniteFramework-cookie-cutter') or directory.startswith('cookie-cutter-'):
                     if target:
                         from_dir = os.path.join(os.getcwd(), '{0}'.format(directory))
                         to_dir = os.path.abspath(os.path.expanduser(target))
@@ -99,7 +99,7 @@ class NewCommand(Command):
                         os.rename(
                             os.path.join(os.getcwd(), '{0}'.format(directory)), os.getcwd() + '/' + name)
                     self.info('\nApplication Created Successfully!\n\nNow just cd into your project and run\n\n    $ craft install\n\nto install the project dependencies.\n\nCreate Something Amazing!')
-
+                    return
             else:
                 self.comment('Could Not Create Application :(')
         else:
